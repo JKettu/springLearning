a9e7badd05f5e712aee045f87a9352727de86385
Nataliya.I.Lebedeva <nataliya.i.lebedeva@rt.ru>
2020-01-16 16:10:54 +0300

    add responsebody

--- src/main/java/connectionKotlin/db/controller/DbController.kt
+++ src/main/java/connectionKotlin/db/controller/DbController.kt
@@ -6,28 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @RequestMapping(path = ["/db"])
 class DbController (@Autowired private val contactService: ContactService) {
 
     @GetMapping(path = ["/getAllContacts"])
-    fun getAllContacts(): List<ContactTest>? {
+    @ResponseBody fun getAllContacts(): List<ContactTest>? {
         return contactService.getAllContactTest()
     }
 
     @GetMapping(path = ["/getContactTestById"])
-    fun getContactTestById(id: Long): ContactTest? {
+    @ResponseBody fun getContactTestById(id: Long): ContactTest? {
         return contactService.getContactTestById(id)
     }
 
     @GetMapping(path = ["/getContactTestByFirstName"])
-    fun getContactTestByFirstName(firstName: String): List<ContactTest>? {
+    @ResponseBody fun getContactTestByFirstName(firstName: String): List<ContactTest>? {
         return contactService.getContactTestByFirstName(firstName)
     }
 
     @GetMapping(path = ["/createContactTest"])
-    fun createContactTest(firstName: String, lastName: String): ContactTest {
+    @ResponseBody fun createContactTest(firstName: String, lastName: String): ContactTest {
         val contact = ContactTest()
         contact.firstName = firstName
         contact.lastName = lastName
@@ -35,12 +36,12 @@ class DbController (@Autowired private val contactService: ContactService) {
     }
 
     @GetMapping(path = ["/deleteContactTestById"])
-    fun deleteContactTestById(id: Long) {
+    @ResponseBody fun deleteContactTestById(id: Long) {
        contactService.deleteContactTestById(id)
     }
 
     @GetMapping(path = ["/deleteAllContactTest"])
-    fun deleteAllContactTest() {
+    @ResponseBody fun deleteAllContactTest() {
         contactService.deleteAllContactTest()
     }
 }
